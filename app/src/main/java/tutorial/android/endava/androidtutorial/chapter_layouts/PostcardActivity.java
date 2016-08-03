234cbe0291208e755f78ec8947c21357ddf17d87
Adina Mihaescu <adina.mihaescu@endava.com>
2016-08-03 14:40:56 +0300

    - add postcard list and post card details activities -> to exemplify  LinearLayout an RelativeLayout

---
+++ app/src/main/java/tutorial/android/endava/androidtutorial/chapter_layouts/PostcardActivity.java
@@ -0,0 +1,15 @@
+package tutorial.android.endava.androidtutorial.chapter_layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tutorial.android.endava.androidtutorial.R;
+
+public class PostcardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postcard);
+    }
+}
---
+++ app/src/main/java/tutorial/android/endava/androidtutorial/chapter_layouts/PostcardDetailsActivity.java
@@ -0,0 +1,30 @@
+package tutorial.android.endava.androidtutorial.chapter_layouts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import tutorial.android.endava.androidtutorial.R;
+
+public class PostcardDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postcard_details);
+
+        TextView postcardTextView = (TextView) findViewById(R.id.tv_postcard_text);
+        TextView destinationTextView = (TextView) findViewById(R.id.tv_postcard_destination);
+        TextView dateTextView = (TextView) findViewById(R.id.tv_postcard_date);
+
+        // make the textview scrollable
+        postcardTextView.setMovementMethod(new ScrollingMovementMethod());
+        //set the destination and the date of the postcard
+        String destination = String.format(getResources().getString(R.string.postcard_details_destination), "Paris");
+        destinationTextView.setText(destination);
+        String date = String.format(getResources().getString(R.string.postcard_details_date), "11/11/2015");
+        dateTextView.setText(date);
+
+    }
+}
