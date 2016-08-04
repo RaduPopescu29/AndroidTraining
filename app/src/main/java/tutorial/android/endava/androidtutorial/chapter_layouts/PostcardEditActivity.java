b546180dc99d783c6f935efb968b9614d28ca04e
Radu Popescu <radu.popescu@endava.com>
2016-08-04 17:45:15 +0300

    removed unnecessary check

--- app/src/main/java/tutorial/android/endava/androidtutorial/chapter_layouts/PostcardEditActivity.java
+++ app/src/main/java/tutorial/android/endava/androidtutorial/chapter_layouts/PostcardEditActivity.java
@@ -1,16 +1,10 @@
 package tutorial.android.endava.androidtutorial.chapter_layouts;
 
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Spanned;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -81,9 +75,7 @@ public class PostcardEditActivity extends AppCompatActivity {
         Intent intent = new Intent();
         Bundle bundle = new Bundle();
         bundle.putSerializable(BUNDLE_POSTCARD_DATA, mPostcard);
-        if (bundle != null) {
-            intent.putExtras(bundle);
-        }
+        intent.putExtras(bundle);
         setResult(RESULT_OK, intent);
         finish();
     }
